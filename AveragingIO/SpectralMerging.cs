1d78de2fe62d323ddb25209cd3fd9cf2c07357c2
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2022-08-23 16:37:10 -0500

    Updated projects to separate spectral math from all other components

--- AveragingIO/SpectralMerging.cs
+++ AveragingIO/SpectralMerging.cs
@@ -1,9 +1,4 @@
 ﻿using MassSpectrometry;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MzLibUtil;
 
 namespace SpectralAveraging
